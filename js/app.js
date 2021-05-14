340b43c14f40e548c3b6cf071bebe8ed07cabd46
Angel Velasco <avelasco@atlantiasearch.com>
2021-05-13 23:38:12 -0500

    Avances en animacion

--- js/app.js
+++ js/app.js
@@ -5,22 +5,39 @@ const app = createApp({
 
     const tl = gsap.timeline()
 
-    let gsapCode = ref('{autoAlpha:0,duration:0.7,scale:0}')
+    let elements = ref([
+      {
+        el: 'p',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci ex dolore tempora quam possimus, earum fugit porro, delectus exercitationem magnam quas corporis, quidem vel velit laudantium harum ut amet! Deleniti!',
+        animation: {}
+      },
+      {
+        el: 'p',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci ex dolore tempora quam possimus, earum fugit porro, delectus exercitationem magnam quas corporis, quidem vel velit laudantium harum ut amet! Deleniti!',
+        animation: {}
+      },
+    ])
+
+    let gsapCode = ref('')
+    let textIndex = ref(0)
 
     const animation = (obj) => {
+      console.log(elements.value)
+      // eval('var test=' + obj);
+      // console.log(test)
 
-      eval('var test=' + obj);
-      console.log(test)
+      // obj = obj.replace('{', '')
+      // obj = obj.replace('}', '')
+      // let data = obj.split(',')
+      // data = data.map(d => d.split(':'))
+      // console.log(data)
+      // obj = Object.fromEntries(data)
 
+      // tl.from('.lorem', obj)
+    }
 
-      obj = obj.replace('{', '')
-      obj = obj.replace('}', '')
-      let data = obj.split(',')
-      data = data.map(d => d.split(':'))
-      console.log(data)
-      obj = Object.fromEntries(data)
-
-      tl.from('.lorem', obj)
+    const currentItem = (index) => {
+      textIndex.value = index
     }
 
     const showCode = () => {
@@ -33,7 +50,10 @@ const app = createApp({
 
     return {
       gsapCode,
-      showCode
+      elements,
+      textIndex,
+      showCode,
+      currentItem
     }
 
   }
